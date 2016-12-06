81a4dfe1f1803f11dc81eff4a1154a648c67452b
MinhMan.Tran <minhman.tran@ogilvy.com>
2016-12-06 13:57:41 +0700

    add simple gallery

---
+++ wordpress/wp-content/plugins/so-widgets-bundle/widgets/simple-gallery/tpl/default.php
@@ -0,0 +1,15 @@
+<?php if( !empty($instance['images']) ) : ?>
+	<div class="sow-simple-gallery-wrapper">
+		<?php
+		foreach( $instance['images'] as $image ) {
+			echo '<div class="sow-simple-gallery-image">';
+			echo '<a href="javascript:void(0);" title="' . $image['title'] . '">';
+			echo wp_get_attachment_image( $image['image'], $instance['display']['attachment_size'], false, array(
+				'title' => $image['title']
+			) );
+			echo '</a>';
+			echo '</div>';
+		}
+		?>
+	</div>
+<?php endif; ?>
