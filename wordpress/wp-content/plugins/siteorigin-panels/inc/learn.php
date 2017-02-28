5529d58541444bce6b8fa29f21d5254ff8076b1d
MinhMan.Tran <minhman.tran@ogilvy.com>
2017-02-28 15:59:12 +0700

    adding new css

--- wordpress/wp-content/plugins/siteorigin-panels/inc/learn.php
+++ wordpress/wp-content/plugins/siteorigin-panels/inc/learn.php
@@ -1,59 +1,39 @@
 <?php
 
-class SiteOrigin_Panels_Courses {
+class SiteOrigin_Panels_Learn {
 
-	function __construct() {
-		add_action( 'siteorigin_panels_courses', array( $this, 'courses' ) );
+	static function display_learn_button(){
+		return siteorigin_panels_setting( 'display-learn' ) &&
+		       apply_filters( 'siteorigin_panels_learn', true );
 	}
 
-	static function single(){
-		static $single;
-		return empty( $single ) ? ( $single = new self() ) : $single;
-	}
-
-	function courses( $courses ){
-		$courses[ 'seo' ] = array(
-			'button' => __( 'SEO', 'siteorigin-panels' ),
-			'title' => __( 'Page Builder SEO', 'siteorigin-panels' ),
-			'text' => __( 'Learn Page Builder SEO.', 'siteorigin-panels' ),
-			'id' => '71ccd71c07'
-		);
-
-		$courses[ 'tips' ] = array(
-			'button' => __( 'Tips', 'siteorigin-panels' ),
-			'title' => __( 'Page Builder Tips', 'siteorigin-panels' ),
-			'text' => __( '12 tips every Page Builder user should know.', 'siteorigin-panels' ),
-			'id' => '300cd058f8'
-		);
-
-		return $courses;
-	}
-
-	function get_course(){
-		static $course;
-		if( ! empty( $course ) ) return $course;
-
-		static $courses;
-		if( empty( $courses ) ){
-			$courses = apply_filters( 'siteorigin_panels_courses', array( ) );
-		}
-
-		if( empty( $courses ) ) return false;
-
-		$ids = array_keys( $courses );
-		$i = floor( time() / (20*60) ) % count( $ids );
-		$course = $courses[ $ids[ $i ] ];
+	/**
+	 * Get the learning URL
+	 *
+	 * @param bool $lesson
+	 * @param bool $category
+	 *
+	 * @return string
+	 */
+	static function get_url( $lesson = false, $category = false ){
+		$learn_url = 'https://siteorigin.com/wp-admin/admin-ajax.php?action=lesson_explore';
 
 		$user = wp_get_current_user();
-		$signup_url = add_query_arg( array(
-			'email' => $user->user_email,
-			'name' => $user->first_name,
-		), 'https://siteorigin.com/wp-admin/admin-ajax.php?action=course_signup_form&course=' . urlencode( $course[ 'id' ] ) );
+		if( $user ) {
+			$learn_url = add_query_arg( array(
+				'email' => $user->user_email,
+				'name' => $user->first_name,
+			), $learn_url );
+		}
 
-		$course['url'] = $signup_url;
+		if( $lesson ) {
+			$learn_url .= '#lesson-' . urlencode( $lesson );
+		}
+		elseif ( $category ) {
+			$learn_url .= '#category-' . urlencode( $category );
+		}
 
-		return $course;
+		return $learn_url;
 	}
-}
 
-SiteOrigin_Panels_Courses::single();
+}
